7769daab52c55f7ab766c738aa27afbd135a5383
Oluwayemi Ogunbameru <yemio@d1stkfactory>
2015-02-24 02:38:54 +0000

    This is test 1 Android.java

---
+++ Android.java
@@ -0,0 +1,28 @@
+//Yemi Ogunbameru
+//Comp Science Test 1
+
+
+
+public class Android {
+	public String make;
+	public String color;
+	public String model;
+	public int year;
+/*----------------------------------------------------------*/
+	Android (){
+		make="";
+		model="";
+		color="";
+		year=0;	
+	}
+	Android ( String thisMake, String thisColor, String thisModel, int thisYear){
+		make=thisMake;
+		model=thisModel;
+		color=thisColor;
+		year=thisYear;
+	}
+public static void main(String[] arg){
+	System.out.println("TESTING");
+}
+
+}
